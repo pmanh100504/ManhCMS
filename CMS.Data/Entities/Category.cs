173c166d0c852a7471a45ea9a2a875a63aac8bc0
pmanh100504 <pmanh1005@gmail.com>
2026-05-15 13:29:18 +0700

    Add project files.

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿//sinh vien: pham van manh
+//mssv:2122110255
+//ngay tao:15/5/26
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // Tên danh mục (vd: Tin Giáo Dục)
+        public string Description { get; set; }
+
+        // Quan hệ: Một danh mục có nhiều bài viết
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
+
